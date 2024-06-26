aca4e68cfb4322741a4ffe1ef047d426320d356b
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-06-26 06:53:28 +0530

    adding spring-6-di project to the repository

---
+++ src/main/java/sunil/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,13 @@
+package sunil.springframework.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiApplication.class, args);
+	}
+
+}
