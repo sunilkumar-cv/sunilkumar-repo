1c75bde88778884d4d7071eae532ea80d5b6373a
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-06-26 07:41:31 +0530

    testing beans created in spring context

--- src/main/java/sunil/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/sunil/springframework/spring6di/Spring6DiApplication.java
@@ -2,12 +2,19 @@ package sunil.springframework.spring6di;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import sunil.springframework.spring6di.controllers.MyController;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring6DiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("In Main method...");
+		System.out.println(controller.sayHello());
 	}
 
 }
--- src/test/java/sunil/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/sunil/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package sunil.springframework.spring6di;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import sunil.springframework.spring6di.controllers.MyController;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+	
+	@Autowired
+	MyController controller;
+
+	@Test
+	void testAutowireOfMyController() {
+		System.out.println(controller.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromContext() {
+		MyController controller = applicationContext.getBean(MyController.class);
+		System.out.println(controller.sayHello());
+	}
 
 	@Test
 	void contextLoads() {
