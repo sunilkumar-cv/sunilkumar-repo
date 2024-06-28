b27ba73b1c65a18f7e06493f82b503ce378fdf67
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-06-28 09:01:38 +0530

    demonstrating spring profiles and default profile

---
+++ src/main/java/sunil/springframework/spring6di/services/i18n/EnglishGreetingService.java
@@ -0,0 +1,16 @@
+package sunil.springframework.spring6di.services.i18n;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import sunil.springframework.spring6di.services.GreetingService;
+
+@Profile({"EN", "default"})
+@Service("i18NService")
+public class EnglishGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World - EN !!!";
+    }
+}
---
+++ src/main/java/sunil/springframework/spring6di/services/i18n/SpanishGreetingService.java
@@ -0,0 +1,14 @@
+package sunil.springframework.spring6di.services.i18n;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+import sunil.springframework.spring6di.services.GreetingService;
+
+@Profile("ES")
+@Service("i18NService")
+public class SpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting() {
+        return "Hola Mundo - ES";
+    }
+}
