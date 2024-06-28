fd23514592019088d4453872b08786db5e590ff6
sunilkumar-cv <sunilkumarreddy.cv@gmail.com>
2024-06-28 10:03:56 +0530

    adding default profile for the assignment

--- src/main/java/sunil/springframework/spring6di/services/assignment/FauxServiceDev.java
+++ src/main/java/sunil/springframework/spring6di/services/assignment/FauxServiceDev.java
@@ -3,7 +3,7 @@ package sunil.springframework.spring6di.services.assignment;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"DEV"})
+@Profile({"DEV", "default"})
 @Service("fauxService")
 public class FauxServiceDev implements FauxService {
     @Override
